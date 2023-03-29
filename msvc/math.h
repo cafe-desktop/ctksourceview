d8e5a5982643b6b526bae01da789b92e58b90496
Pablo Barciela <scow@riseup.net>
2023-03-29 11:24:42 +0200

    rename CDK-Pixbuf -> GDK-Pixbuf

--- msvc/math.h
+++ msvc/math.h
@@ -1,6 +1,6 @@
 /* GdkPixbuf library - Compatibility Math Header for
  *                     pre-Visual Studio 2013 Headers
- *                     to Build CDK-Pixbuf
+ *                     to Build GDK-Pixbuf
  *
  * Copyright (C) 2014 Chun-wei Fan
  *
